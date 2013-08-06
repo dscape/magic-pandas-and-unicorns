f3de46a0f254001e5486ee4b3c95687a61067c9a
Nuno Job <nunojobpinto@gmail.com>
2013-08-06 23:57:58 +0100

    Reformat stuff

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 var express = require('express'),
-qs = require('querystring'),
-app = express(),
-db = require('./lib/db')(),
-scraper = require('./lib/scraper');
+  qs = require('querystring'),
+  app = express(),
+  db = require('./lib/db')(),
+  scraper = require('./lib/scraper');
+
 //
 // start scraping
 //
 scraper(db);
-app.use(function log(req, res, next)
-{
+app.use(function log(req, res, next) {
   console.log(req.method + ' ' + req.url);
   next();
 });
+
 //
 // gets an athlete by id
 //
-app.get('/athlete/:id', function(req, res)
-{
-  db.get(req.params.id, function(err, athlete)
-  {
-    if (err)
-    {
+app.get('/athlete/:id', function (req, res) {
+  db.get(req.params.id, function (err, athlete) {
+    if (err) {
       //
       // not found
       //
@@ -35,12 +33,10 @@ app.get('/athlete/:id', function(req, res)
     res.json(200, athlete);
   });
 });
-app.get('/search', function(req, res)
-{
-  db.range(req.query, function(err, results)
-  {
-    if (err)
-    {
+
+app.get('/search', function (req, res) {
+  db.range(req.query, function (err, results) {
+    if (err) {
       return res.json(500, {
         error: 'Bad search'
       });
@@ -48,6 +44,7 @@ app.get('/search', function(req, res)
     res.json(200, (results || []));
   });
 });
+
 //
 // /u17/f/60
 // 
@@ -55,8 +52,6 @@ app.get('/search', function(req, res)
 // investigate levelup (documentation) to find how to support this query
 // give it back to the users
 //
-app.get('/:agegroup/:sex/:distance', function(req, res)
-{
-});
+app.get('/:agegroup/:sex/:distance', function (req, res) {});
 app.listen(process.env.PORT || 3000);
 console.log('working in 3000');
