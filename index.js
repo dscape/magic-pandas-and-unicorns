367c460cacb637bc5af09e8a77b7c49aa02d2a99
Nuno Job <nunojobpinto@gmail.com>
2013-08-08 02:35:00 +0100

    seems to work now

--- index.js
+++ index.js
@@ -9,14 +9,15 @@ var express = require('express'),
 //
 scraper(db);
 
+//
+// cors overides
+//
 app.use(express.methodOverride());
-
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-  // intercept OPTIONS method
   if ('OPTIONS' == req.method) {
     res.send(200);
   } else {
@@ -24,6 +25,9 @@ app.use(function (req, res, next) {
   }
 });
 
+//
+// log each request
+//
 app.use(function log(req, res, next) {
   console.log(req.method + ' ' + req.url);
   next();
@@ -50,36 +54,19 @@ app.get('/athlete/:id', function (req, res) {
   });
 });
 
-app.get('/search', function (req, res) {
-  db.range(req.query, function (err, results) {
-    if (err) {
-      return res.json(500, {
-        error: 'Bad search'
-      });
-    }
-    res.json(200, (results || []));
-  });
-});
-
 //
-// /u17/f/60
+// Simple composable queries
+// only for top level properties
 //
-app.get('/:age_group/:gender/:distance', function (req, res) {
-  db.range(req.params, function (err, results) {
-    if (err) {
-      return res.json(500, {
-        error: 'Bad search'
-      });
-    }
-    res.json(200, (results || []));
-  });
-});
-
+// samples:
 //
-// /u17/f/60/2009
+// curl localhost:3000/range?gender=Female\&id=1
+// curl localhost:3000/range?gender=Female\&id=30
+// curl localhost:3000/range?where=outdoors
+// curl localhost:3000/range?id=1
 //
-app.get('/:age_group/:gender/:distance/:year', function (req, res) {
-  db.range(req.params, function (err, results) {
+app.get('/range', function (req, res) {
+  db.query(req.query, function (err, results) {
     if (err) {
       return res.json(500, {
         error: 'Bad search'
