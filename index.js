601eb82f1b1670799a82404d22122ade4f717d61
Nuno Job <nunojobpinto@gmail.com>
2013-08-08 01:24:12 +0100

    broke all things

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ var express = require('express'),
 // start scraping
 //
 scraper(db);
+
+app.use(express.methodOverride());
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+});
+
 app.use(function log(req, res, next) {
   console.log(req.method + ' ' + req.url);
   next();
@@ -47,11 +63,31 @@ app.get('/search', function (req, res) {
 
 //
 // /u17/f/60
-// 
 //
-// investigate levelup (documentation) to find how to support this query
-// give it back to the users
+app.get('/:age_group/:gender/:distance', function (req, res) {
+  db.range(req.params, function (err, results) {
+    if (err) {
+      return res.json(500, {
+        error: 'Bad search'
+      });
+    }
+    res.json(200, (results || []));
+  });
+});
+
 //
-app.get('/:agegroup/:sex/:distance', function (req, res) {});
+// /u17/f/60/2009
+//
+app.get('/:age_group/:gender/:distance/:year', function (req, res) {
+  db.range(req.params, function (err, results) {
+    if (err) {
+      return res.json(500, {
+        error: 'Bad search'
+      });
+    }
+    res.json(200, (results || []));
+  });
+});
+
 app.listen(process.env.PORT || 3000);
 console.log('working in 3000');
